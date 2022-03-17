22be579876bcfe54e4ec24e1ff63f09820a5b233
osazeejedi <oghagbonosazee20@gmail.com>
2022-03-17 23:34:56 +0100

    contract

---
+++ contracts/Merkletree_dist.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+
+contract Merkle is ERC20{
+    address _owner= msg.sender;
+    uint256 _supply= 1000000000000000000000;
+   
+    constructor() ERC20("JEDI", "JD") 
+        public
+    { 
+        _mint(_owner, _supply);
+    }
+
+//lets create our markle-tree distributor.
+
+
+ bytes32 public Merkleroot = 0x8f55b2d91532b9c79edf5aaa1b303680f3411bdb90730695dedf18bb0c78f14a;
+
+
+
+function claimToken (bytes32[] calldata proof, address claimer, uint256 amount) public {
+
+    bytes32 leaf = keccak256(abi.encodePacked(claimer));
+    require (MerkleProof.verify(proof, Merkleroot, leaf), 'invalid proof');
+
+    _mint (claimer, amount);
+
+
+
+}
+
+
+
+
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,36 @@
+
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Dist = await ethers.getContractFactory("Merkle");
+  const dist = await Dist.deploy();
+
+  await dist.deployed();
+
+  console.log("dist deployed to:", dist.address);
+
+  const claimer = "0x0F188eCe555d848936b968DD0911200F79CE44F6";
+  const amount = 10000000000000000000;
+
+  const getToken= await dist.claimToken(['0xeeec69a26eecbc1b4fcd220601e5f30022abbf6cd2e95ee090e75941b2db1f1d'], claimer, amount)
+
+
+
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
